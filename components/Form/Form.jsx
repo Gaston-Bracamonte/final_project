8d7f2a6da2c324b644525441005b307e5f7245f6
Gaston <gaston13@gmail.com>
2024-04-07 19:05:04 -0300

    Componente formulario

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -3,25 +3,20 @@ import "./Form.css";
 
 const Form = () => {
     return(
-        <div className="form">
-            <div>
-                <h2>Formulario de Contacto</h2>
-                <form action="procesar_formulario.php" method="post">
+        <div className="form-container">
+            <div className="form-wrapper">
+                <h2>¡Contáctanos!</h2>
+                <form>
                     <label htmlFor="nombre">Nombre:</label>
-                    <input type="text" id="nombre" name="nombre" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+" required />
-                    <br /><br />
+                    <input type="text" id="nombre" name="nombre" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+" required placeholder="Ingrese su nombre" />
                     <label htmlFor="apellido">Apellido:</label>
-                    <input type="text" id="apellido" name="apellido" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+" required />
-                    <br /><br />
+                    <input type="text" id="apellido" name="apellido" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+" required  placeholder="Ingrese su apellido" />
                     <label htmlFor="telefono">Teléfono:</label>
-                    <input type="tel" id="telefono" name="telefono" pattern="[0-9]+" required />
-                    <br /><br />
+                    <input type="number" id="telefono" name="telefono" pattern="[0-9]+" required  placeholder="Ingrese su teléfono" />
                     <label htmlFor="email">E-mail:</label>
-                    <input type="email" id="email" name="email" required />
-                    <br /><br />
+                    <input type="text" id="email" name="email" required  placeholder="Ingrese su correo electrónico" />
                     <label htmlFor="mensaje">Detalle del Mensaje:</label>
-                    <textarea id="mensaje" name="mensaje" rows="4" required></textarea>
-                    <br /><br />
+                    <textarea id="mensaje" name="mensaje" rows="4" required  placeholder="Ej: quiero saber más sobre Villa Carlos Paz"></textarea>
                     <input type="submit" value="Enviar" />
                 </form>
             </div>
