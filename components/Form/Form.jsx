7d009393e7e5c80c10e978d86fba7330c94c03c2
Gaston <gaston13@gmail.com>
2024-04-16 19:28:59 -0300

    APIs terminadas

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -5,7 +5,7 @@ import MessageAlert from "../MessageAlert/MessageAlert.jsx";
 
 const Form = () => {
     const [showModal, setShowModal] = useState(false);
-    const [isSending, setIsSending] = useState(false); // New state for sending
+    const [isSending, setIsSending] = useState(false);
 
     //Validamos si el correo ingresado ya está en nuestra base de datos
     const validation_mail = async (email) => {
@@ -15,23 +15,49 @@ const Form = () => {
                 method: 'POST',
                 body: JSON.stringify({ email })
             });
-
+            //Si recibimos respuesta, procedemos a la lógica dentro del formulario
             if (response.ok) {
-                console.log(response);
-                setShowModal(true);
+
+                const responseData = await response.json(); // Parse response JSON
+
+                //Validamos si recibimos un id en la respuesta
+                if (responseData.id) {
+
+                    // Seteamos los input con la información obtenida desde la base de datos
+                    document.getElementById('firstname').value = responseData.firstname;
+                    document.getElementById('lastname').value = responseData.lastname;
+                    document.getElementById('phone').value = responseData.phone;
+                    document.getElementById('user_id').value = responseData.id;
+
+                    // Habilitamos el elemento message y el botón para enviar el formulario
+                    var waitingElements = document.querySelectorAll('.responseValidate');
+                    waitingElements.forEach(function (element) {
+                        element.classList.add('responseOk');
+                        element.classList.remove('waitingResponse');
+                    });
+
+                }
+                // Al no tener el mail en la base datos, habilitamos para completar todos los campos y el botón de enviar.
+                else {
+                    var waitingElements = document.querySelectorAll('.waitingResponse');
+                    waitingElements.forEach(function (element) {
+                        element.classList.add('responseOk');
+                    });
+                }
+                           
             } else {
-                // Handle errors
+                // Manejo de errores en el formulario
                 console.error('Error sending form:', response.statusText);
             }
         } catch (error) {
-            // Handle network or other errors
+            // Manejo de errores de conexión
             console.error('Error sending form:', error);
         } finally {
             setIsSending(false);
         }
     }
 
-    //Enviamos la información del formulario a la API
+    //Enviamos la información del formulario a la API encargada de registrar el mensaje y la persona, si correspondiera
     const handleClickEnviar = async (event) => {
         event.preventDefault();
         const form = document.getElementById('contactForm');
@@ -44,19 +70,13 @@ const Form = () => {
                 method: 'POST',
                 body: formData
             });
-
+            //Validamos que hayamos obtenido respuesta de la llamada de la API
             if (response.ok) {
                 console.log(response);
-                var waitingElements = document.querySelectorAll('.waitingResponse');
-                waitingElements.forEach(function(element) {
-                    element.classList.add('responseOk');
-                });
+
                 setShowModal(true);
             } else {
                 // Handle errors
-                var waitingElements = document.querySelectorAll('.waitingResponse');
-                waitingElements.forEach(function(element) {
-                    element.classList.add('responseOk');})
                 console.error('Error sending form:', response.statusText);
             }
         } catch (error) {
@@ -73,18 +93,19 @@ const Form = () => {
         <div>
             <div className="form-container">
                 <div className="form-wrapper">
-                <form id="contactForm">
+                    <form id="contactForm">
+                        <input type="hidden" id="user_id" value="" name="user_id" />
                         <label htmlFor="email">E-mail:</label>
-                        <input type="text" id="mail" name="email" required placeholder="Ingrese su correo electrónico" onChange={(event) => validation_mail(event.target.value)}/>
+                        <input type="text" id="email" name="email" required placeholder="Ingrese su correo electrónico" onBlur={(event) => validation_mail(event.target.value)} />
                         <label htmlFor="nombre">Nombre:</label>
-                        <input className="waitingResponse responseOk" type="text" id="firstname" name="firstname" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+" required placeholder="Ingrese su nombre"/>
+                        <input className="waitingResponse" type="text" id="firstname" name="firstname" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+" required placeholder="Ingrese su nombre" />
                         <label htmlFor="apellido">Apellido:</label>
-                        <input className="waitingResponse" type="text" id="lastname" name="lastname" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+" required placeholder="Ingrese su apellido"/>
+                        <input className="waitingResponse" type="text" id="lastname" name="lastname" pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+" required placeholder="Ingrese su apellido" />
                         <label htmlFor="telefono">Teléfono:</label>
-                        <input className="waitingResponse" type="number" id="phone" name="phone" pattern="[0-9]+" required placeholder="Ingrese su teléfono"/>
+                        <input className="waitingResponse" type="number" id="phone" name="phone" pattern="[0-9]+" required placeholder="Ingrese su teléfono" />
                         <label htmlFor="mensaje">Detalle del Mensaje:</label>
-                        <textarea className="waitingResponse" id="message" name="message" rows="4" required placeholder="Ej: quiero saber más sobre Villa Carlos Paz"></textarea>
-                        <button onClick={handleClickEnviar} className="waitingResponse" disabled={isSending}>
+                        <textarea className="waitingResponse responseValidate" id="message" name="message" rows="4" required placeholder="Ej: quiero saber más sobre Villa Carlos Paz"></textarea>
+                        <button onClick={handleClickEnviar} className="waitingResponse responseValidate" id="submit-button" disabled={isSending}>
                             {isSending ? 'Enviando...' : 'Enviar'}
                         </button>
                         {showModal && ReactDOM.createPortal(<MessageAlert title="Mensaje" message="Su mensaje ha sido enviado" />, elementoModal)}
