b2a873e0e47699ba279846374fd71988fde14e3b
Gaston <gaston13@gmail.com>
2024-04-18 01:33:17 -0300

    Cambiada imágenes slider

--- components/Slider/Slider.jsx
+++ components/Slider/Slider.jsx
@@ -3,11 +3,12 @@ import React, { useState } from 'react';
 const Slider = () => {
   // Array de URLs de imágenes de ejemplo
   const images = [
-    '../../src/img/slider_img/night_city.jpg',
-    '../../src/img/slider_img/nono.jpg',
-    '../../src/img/slider_img/park.jpg',
-    '../../src/img/slider_img/san_antonio_lake.jpg',
-    '../../src/img/slider_img/sierras.jpg',
+    'https://i.pinimg.com/originals/9f/d2/53/9fd253256bdac729155011c1c3bd007a.jpg',
+    'https://www.cordobaturismo.gov.ar/wp-content/uploads/2023/01/yucac10.jpg',
+    'https://i.pinimg.com/originals/e8/ff/c1/e8ffc1187f8a3fb820a67c08204c9144.jpg',
+    'https://guiadeturismoargentina.com/wp-content/uploads/2019/04/carlos-paz2.jpg',
+    'https://i.pinimg.com/originals/7f/a7/1b/7fa71be85a25fc6deb61f4f08f37c7bb.jpg',
+    'https://www.cordobaturismo.gov.ar/wp-content/uploads/2018/09/16707573_10155792909783840_5514747335741479490_o.jpg',
   ];
 
   // Estado para almacenar el índice de la imagen actual
